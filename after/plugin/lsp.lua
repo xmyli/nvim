bff5df4af8f7216d59e8e81b430ef9f3ea652c07
xmyli <devxmyli@gmail.com>
2023-05-11 14:44:54 +0800

    Update key mapping for selecting LSP suggestion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,3 +8,14 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+})
